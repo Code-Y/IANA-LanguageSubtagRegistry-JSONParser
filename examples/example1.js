04484db05dd15b640d3eb5ac6fece1af4efac028
hitmands <mandatog@MOBCTEK838.yoox.net>
2016-03-25 17:35:52 +0100

    provided basic working example

--- examples/example1.js
+++ examples/example1.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
-var adapter = require('../TextToJsonAdapter');
-var filename = '20160321_language-subtag-registry';
-const ENCODING = 'utf-8';
+var IANAParser = require('../index');
 
-var data = fs.readFileSync(`./data/${filename}.txt`, ENCODING);
-var result = adapter(data);
-
-fs.writeFileSync(
-  `./dist/${filename}.json`,
-  JSON.stringify(result, null, 2),
-  {encoding: ENCODING}
-);
+IANAParser
+  .fetchRegistry()
+  .then(result => JSON.stringify(result, null, 2))
+  .then((data) => {
+    return fs.writeFileSync(
+      `./dist/result.json`,
+      data,
+      {encoding: 'utf-8'}
+    );
+  })
+;
